640ed4f507870f2d72ece7da8be2cdb9de547096
github username <github email address>
2018-03-06 17:22:28 -0500

    styled search bar and buttons on photos

--- frontend-final-project/src/components/TagList/TagSearch.js
+++ frontend-final-project/src/components/TagList/TagSearch.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { connect } from "react-redux";
+import '../../CSS/Tag.css';
 
 
 class TagSearch extends React.Component {
 
   render = () => {
     return (
-      <div className="tag-search">
-        <input list="data" type="text" onChange={this.props.handleSearch} placeholder="Search Tags"/>
-        <datalist id="data" >
+      <div className="tag-search-container">
+        <input id="search-tags" list="search-tag-data" type="text" onChange={this.props.handleSearch} placeholder="Search Tags"/>
+        <datalist id="search-tag-data" >
             {this.props.tags.map( (t) => {
                 let tagKey = `search-${t.id}`
                 return <option value={t.name} key={tagKey}/>
             })}
         </datalist>
+        <i class="material-icons" id="search-submit">search</i>
       </div>
     )
   }
